1a29bb3cfae4713a87bd35aebe13987e83ac39c2
Almir Pečenković <almir.pecenkovic333gmail.com>
2023-03-04 20:51:08 +0100

    json, add posts

--- projects/my-app/src/app/app-demo/json-placeholder-demo/modules/posts/json-posts/json-posts.component.ts
+++ projects/my-app/src/app/app-demo/json-placeholder-demo/modules/posts/json-posts/json-posts.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+// ANGULAR
+import { Component, Input} from '@angular/core';
+
+// API
+import { JSONComment } from '../../../models/json-comment.model';
+import { AngularMaterialIconCodeENUM } from 'my-library';
+
 
 @Component({
   selector: 'app-json-posts',
@@ -6,5 +12,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./json-posts.component.scss']
 })
 export class JsonPostsComponent {
+  // ENUMS
+  public get iconCodeENUM(): typeof AngularMaterialIconCodeENUM {
+    return AngularMaterialIconCodeENUM;
+  }
 
+  // VAR
+  @Input() public avatarUrl: string;
+  @Input() public comments: JSONComment[] = [];
+  public isLiked: boolean = false;
+
+  public get avatarDisplay() {
+    return !!this.avatarUrl ? this.avatarUrl : this.defaultAvatarUrl;
+  }
+
+  private defaultAvatarUrl: string =
+  "assets/img/demo/avatars/noavatar.png";;
+
+  constructor() {}
+
+  public like() {
+    this.isLiked = !this.isLiked;
+  }
 }
--- projects/my-app/src/app/app-demo/json-placeholder-demo/modules/posts/json-posts/json-posts.module.ts
+++ projects/my-app/src/app/app-demo/json-placeholder-demo/modules/posts/json-posts/json-posts.module.ts
@@ -1,16 +1,35 @@
+// ANGULAR
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// API
 import { JsonPostsRoutingModule } from './json-posts-routing.module';
 import { JsonPostsComponent } from './json-posts.component';
+
+// ANGULAR MATERIAL
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+// 3lib
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { TranslateModule } from '@ngx-translate/core';
 
 
 @NgModule({
   declarations: [
     JsonPostsComponent
   ],
+  exports:[
+    JsonPostsComponent
+  ],
   imports: [
     CommonModule,
+    FlexLayoutModule,
+    TranslateModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
     JsonPostsRoutingModule
   ]
 })
