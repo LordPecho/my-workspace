9fc5930d6cac47d774d74854f094551d3d513a3f
Almir Pečenković <almir.pecenkovic333gmail.com>
2023-03-04 20:58:05 +0100

    json, add comment-overview

--- projects/my-app/src/app/app-demo/json-placeholder-demo/modules/comments/json-comment-overview/json-comment-overview.component.ts
+++ projects/my-app/src/app/app-demo/json-placeholder-demo/modules/comments/json-comment-overview/json-comment-overview.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+// ANGULAR
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnDestroy,
+  SimpleChanges,
+} from '@angular/core';
+
+// RXJS
+import { Subscription } from 'rxjs';
+
+//SERVICE
+import { JsonCommentService } from '../../../services/json-comment.service';
+
+//API
+import { JSONComment } from '../../../models/json-comment.model';
 
 @Component({
   selector: 'app-json-comment-overview',
   templateUrl: './json-comment-overview.component.html',
   styleUrls: ['./json-comment-overview.component.scss']
 })
-export class JsonCommentOverviewComponent {
+export class JsonCommentOverviewComponent implements OnDestroy,OnChanges {
+  // VAR
+  @Input() public postId: number;
+  @Input() public avatarUrl: string;
+  public comments: JSONComment[] = [];
 
+  // SUBSCRIPTION
+  private subscription: Subscription = new Subscription();
+
+  constructor(private jsonCommentService: JsonCommentService) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (!!changes['postId'].currentValue) {
+      this.subscription.add(
+        this.jsonCommentService
+          .getComments()
+          .subscribe((comments: JSONComment[]) => {
+            this.comments = [];
+            comments.forEach((comment: JSONComment) => {
+              const sanitizedCommentJson = new JSONComment(comment);
+              if (comment.postId == this.postId)
+                this.comments.push(sanitizedCommentJson);
+            });
+          })
+      );
+    }
+  }
+
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
--- projects/my-app/src/app/app-demo/json-placeholder-demo/modules/comments/json-comment-overview/json-comment-overview.module.ts
+++ projects/my-app/src/app/app-demo/json-placeholder-demo/modules/comments/json-comment-overview/json-comment-overview.module.ts
@@ -1,16 +1,28 @@
+// ANGULAR
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// API
 import { JsonCommentOverviewRoutingModule } from './json-comment-overview-routing.module';
 import { JsonCommentOverviewComponent } from './json-comment-overview.component';
 
+//MODULE
+import { JsonCommentsModule } from '../json-comments/json-comments.module';
+
+//3Lib
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
     JsonCommentOverviewComponent
   ],
+  exports: [
+    JsonCommentOverviewComponent
+  ],
   imports: [
     CommonModule,
+    TranslateModule,
+    JsonCommentsModule,
     JsonCommentOverviewRoutingModule
   ]
 })
