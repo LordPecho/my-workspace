91785d5236d098e5c57c032b234f9a2ea828b240
Almir Pečenković <almir.pecenkovic333gmail.com>
2023-03-04 21:18:08 +0100

    json, add post-mat-card

--- projects/my-app/src/app/app-demo/json-placeholder-demo/modules/posts/json-post-mat-card/json-post-mat-card.component.ts
+++ projects/my-app/src/app/app-demo/json-placeholder-demo/modules/posts/json-post-mat-card/json-post-mat-card.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+// ANGULAR
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+//API
+import { AngularMaterialIconCodeENUM, TranslateKeyENUM } from 'my-library';
+import { JSONUser } from '../../../models/json-user.model';
+import { JSONPost } from '../../../models/json-post.model';
 
 @Component({
   selector: 'app-json-post-mat-card',
@@ -6,5 +13,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./json-post-mat-card.component.scss']
 })
 export class JsonPostMatCardComponent {
+  //ENUMS
+  public get ngxIconCodeENUM(): typeof AngularMaterialIconCodeENUM {
+    return AngularMaterialIconCodeENUM;
+  }
+  public get ngxTranslateKeyENUM(): typeof TranslateKeyENUM {
+    return TranslateKeyENUM;
+  }
 
+  // VAR
+  public postId: number | undefined = undefined;
+
+  @Input() public avatarUrl: string;
+  @Input() public user: JSONUser;
+  @Input() public post: JSONPost;
+  public isLiked: boolean = false;
+
+  @Output() eventEmitter: EventEmitter<number> = new EventEmitter<number>();
+
+  public get avatarDisplay() {
+    return !!this.avatarUrl ? this.avatarUrl : this.defaultAvatarUrl;
+  }
+
+  private defaultAvatarUrl: string =
+  "assets/img/demo/avatars/noavatar.png";;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {}
+  public navigate(user: JSONUser) {
+    this.router.navigate(['users/', user.id], {
+      relativeTo: this.activatedRoute,
+    });
+  }
+
+  // SELECT NEW COMMENT
+  public addNewComment(postId: number) {
+    this.eventEmitter.emit(postId);
+  }
+  public like() {
+    this.isLiked = !this.isLiked;
+  }
 }
--- projects/my-app/src/app/app-demo/json-placeholder-demo/modules/posts/json-post-mat-card/json-post-mat-card.module.ts
+++ projects/my-app/src/app/app-demo/json-placeholder-demo/modules/posts/json-post-mat-card/json-post-mat-card.module.ts
@@ -1,16 +1,35 @@
+//ANGULAR
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+//API
 import { JsonPostMatCardRoutingModule } from './json-post-mat-card-routing.module';
 import { JsonPostMatCardComponent } from './json-post-mat-card.component';
+
+//ANGULAR MATERIAL
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+// 3lib
+import { TranslateModule } from '@ngx-translate/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @NgModule({
   declarations: [
     JsonPostMatCardComponent
   ],
+  exports: [
+    JsonPostMatCardComponent
+  ],
   imports: [
     CommonModule,
+    FlexLayoutModule,
+    TranslateModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
     JsonPostMatCardRoutingModule
   ]
 })
